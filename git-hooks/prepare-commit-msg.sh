a9cbdcbd401c2f9d3c5e2de1c79b3598b4d7d80d
EMEA\MashkovD <dmitry.mashkov@wunderman.com>
2018-04-30 13:43:45 +0200

    test

--- git-hooks/prepare-commit-msg.sh
+++ git-hooks/prepare-commit-msg.sh
@@ -8,7 +8,8 @@ branchName=`git rev-parse --abbrev-ref HEAD`
 jiraId=$(echo $branchName | sed -nr 's,(feature|bugfix|hotfix|release)+/([A-Z]+-[0-9]+)-.+,\1,p')
 
 # only prepare commit message if pattern matched and jiraId was found
-if [[ ! -z $jiraId ]]; then
- # $2 is the name of the file containing the commit message
- sed -i.bak -e "1s/^/\n\n$jiraId: /" $2
-fi
+#if [[ ! -z $jiraId ]]; then
+ # $1 is the name of the file containing the commit message
+# sed -i.bak -e "1s/^/\n\n$jiraId: /" $1
+ sed -i.bak -e "1s/^/\n\nAsdffdsa: /" $1
+#fi
