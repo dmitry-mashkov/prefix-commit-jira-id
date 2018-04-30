1b12cd9342770ecc5b20162b5f5909f483a372e1
EMEA\MashkovD <dmitry.mashkov@wunderman.com>
2018-04-30 13:39:50 +0200

    test

--- git-hooks/prepare-commit-msg.sh
+++ git-hooks/prepare-commit-msg.sh
@@ -4,10 +4,11 @@
 branchName=`git rev-parse --abbrev-ref HEAD`
 
 # search jira issue id in a pattern such a "feature/ABC-123-description"
-jiraId=$(echo $branchName | sed -nr 's,(?:feature|bugfix|hotfix|release)+/([A-Z]+-[0-9]+)-.+,\1,p')
+# SED doesn't like the PCRE non-capturing notation, so I added another capturing brackets for a branch prefix
+jiraId=$(echo $branchName | sed -nr 's,(feature|bugfix|hotfix|release)+/([A-Z]+-[0-9]+)-.+,\1,p')
 
 # only prepare commit message if pattern matched and jiraId was found
 if [[ ! -z $jiraId ]]; then
- # $1 is the name of the file containing the commit message
- sed -i.bak -e "1s/^/\n\n$jiraId: /" $1
+ # $2 is the name of the file containing the commit message
+ sed -i.bak -e "1s/^/\n\n$jiraId: /" $2
 fi
