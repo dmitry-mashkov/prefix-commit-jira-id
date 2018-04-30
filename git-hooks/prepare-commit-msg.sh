6e30588329e7458c4f3f8d1e4039ff34da289f12
EMEA\MashkovD <dmitry.mashkov@wunderman.com>
2018-04-30 13:50:03 +0200

    test

--- git-hooks/prepare-commit-msg.sh
+++ git-hooks/prepare-commit-msg.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo $1
 
 # get current branch
 branchName=`git rev-parse --abbrev-ref HEAD`
@@ -12,7 +11,7 @@ branchName=`git rev-parse --abbrev-ref HEAD`
 jiraId=$(echo $branchName | sed -nr 's,(master),\1,p')
 #jiraId='asdf'
 
-echo $1
+echo $2
 echo $jiraId
 
 # only prepare commit message if pattern matched and jiraId was found
