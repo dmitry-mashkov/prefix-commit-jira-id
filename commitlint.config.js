6c8e4f2af1286282f438fe1de51c571daf52e8c7
EMEA\MashkovD <dmitry.mashkov@wunderman.com>
2018-05-02 13:14:56 +0200

    test

--- commitlint.config.js
+++ commitlint.config.js
@@ -12,7 +12,7 @@ module.exports = {
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'upper-case'],
-    'type-empty': [2, 'never'],
+    'type-empty': [2, 'never']
     /*'type-enum': [
       2,
       'always',
