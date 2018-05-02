55ead97a44ff8f154ec3e13924ba14e88d3fa48e
EMEA\MashkovD <dmitry.mashkov@wunderman.com>
2018-05-02 14:11:46 +0200

    ABC-319: test

--- scripts/commit-msg.js
+++ scripts/commit-msg.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const _ = require('lodash/fp');
+const childProcess = require('child_process');
 
-const JIRA_TAG = 'TAG';
+const MESSAGE_FILE_PATH = process.env.GIT_PARAMS;
 
 /* If message title:
  * * Doesn't start with square brackets []
@@ -17,28 +19,59 @@ const JIRA_TAG = 'TAG';
  */
 
 
+main();
+
+
+function main() {
+  let message = fs.readFileSync(MESSAGE_FILE_PATH, { encoding: 'utf-8' });
+  const branchName = getBranchName();
+  const issueTag = getIssueTagFromBranchName(branchName);
+
+  if (issueTag && !commitMessageIsSpecial(message)) {
+    message = `${issueTag}: ${message}`;
+    fs.writeFileSync(MESSAGE_FILE_PATH, message, { encoding: 'utf-8' });
+  }
+}
+
+function getBranchName() {
+  return childProcess
+    .execSync('git rev-parse --abbrev-ref HEAD', { encoding: 'utf-8' })
+    .split('\n')[0];
+}
+
+function getIssueTagFromBranchName (str) {
+  const jiraTagPattern = /^(?:feature|bugfix|hotfix|release)\/([A-Z]+-\d+)-.+/i;
+  const matched = str.match(jiraTagPattern);
+
+  return matched && matched[1];
+}
+
+function commitMessageIsSpecial(message) {
+  // Skip if starts with braces
+  // if (_.overSome([])(message))
+
+  if (message.match(/^\[[^\]]/)) return false;
+
+  // Skip if starts with standard merge message
+  if (
+    _.startsWith(message, 'Merge branch') ||
+    _.startsWith(message, 'Merge pull request')
+  ) return false;
+
+  if (_.startsWith('#')) return false;
+
+  return true;
+}
+
+
+/*
 const startsWithBraces = (str) => str.match(/^\[[^\]]/);
 const startsWithMergeBranch = (str) => str.indexOf('Merge branch') === 0;
 const startsWithMergePR = (str) => str.indexOf('Merge pull request') === 0;
 const startsWithHash = (str) => str.indexOf('#') === 0;
-const isInvalidMessage = (str) =>
-!startsWithBraces(str) && !startsWithMergeBranch(str) && !startsWithMergePR(str) && !startsWithHash(str);
-const tagMatcher = new RegExp(`^${JIRA_TAG}-\\d+`, 'i');
-const getIssueTagFromBranchName = (str) => {
-  const matched = str.match(tagMatcher);
-  return matched && matched[0];
-};
+const isInvalidMessage = (str) => !startsWithBraces(str)
+                               && !startsWithMergeBranch(str)
+                               && !startsWithMergePR(str)
+                               && !startsWithHash(str);
 
-const messageFile = process.env.GIT_PARAMS;
-const message = fs.readFileSync(messageFile, { encoding: 'utf-8' });
-const messageTitle = message.split('\n')[0];
-const branchName = require('child_process').execSync('git rev-parse --abbrev-ref HEAD', { encoding: 'utf-8' }).split('\n')[0];
-const issueTag = getIssueTagFromBranchName(branchName);
-
-if (issueTag && isInvalidMessage(messageTitle)) {
-  // Apply the issue tag to message title
-  const messageLines = message.split('\n');
-  messageLines[0] = `[${issueTag.toUpperCase()}] ${messageTitle}`;
-  fs.writeFileSync(messageFile, messageLines.join('\n'), { encoding: 'utf-8' });
-  console.log(`New message title: ${messageLines[0]}`);
-}
+*/
